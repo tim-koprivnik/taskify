d0a2e4831b5a11208e361ca95ea87761062b1dda
Tim Koprivnik <tim.koprivnik@agiledrop.com>
2023-12-06 19:13:56 +0100

    Add lib

---
+++ lib/create-safe-action.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+export type FieldErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string | null;
+  data?: TOutput;
+};
+
+export const createSafeAction = <TInput, TOutput>(
+  schema: z.Schema<TInput>,
+  handler: (validatedData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+  return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+    const validationResults = schema.safeParse(data);
+
+    if (!validationResults.success) {
+      return {
+        fieldErrors: validationResults.error.flatten()
+          .fieldErrors as FieldErrors<TInput>,
+      };
+    }
+
+    // return handler(validationResults.data);
+    return await handler(validationResults.data);
+  };
+};
