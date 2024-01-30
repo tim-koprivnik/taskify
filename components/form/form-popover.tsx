77119380cb8b180df312e664d631d1a6b4fb6973
TK <t.koprivnik@gmail.com>
2024-01-30 16:34:15 +0100

    Add unsplash

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -15,6 +15,7 @@ import { X } from 'lucide-react';
 import { FieldErrors } from '../../lib/create-safe-action';
 import { createBoard } from '@/actions/create-board';
 import { toast } from 'sonner';
+import { FormPicker } from './form-picker';
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -42,8 +43,11 @@ export const FormPopover = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
+    const image = formData.get('image') as string;
 
-    execute({ title });
+    console.log({ image });
+
+    // execute({ title });
   };
 
   return (
@@ -68,6 +72,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board Title"
